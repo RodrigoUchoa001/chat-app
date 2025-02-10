68836681aa8f6ff9349dab6ad58bffce43bbeeba
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-02-09 22:16:08 -0300

    initializing firebase and riverpod on main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'package:chatapp/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    ProviderScope(
+      child: MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
