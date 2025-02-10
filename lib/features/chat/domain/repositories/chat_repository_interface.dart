20df8218f93ca2797f2fb9409a6f94d2e118a537
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-02-10 12:46:35 -0300

    created ChatRepositoryInterface

---
+++ lib/features/chat/domain/repositories/chat_repository_interface.dart
@@ -0,0 +1,11 @@
+import 'package:chatapp/features/chat/data/dto/chat_dto.dart';
+
+abstract interface class ChatRepositoryInterface {
+  Stream<List<ChatDTO>?> getChats();
+  Stream<List<MessageDTO>?> getMessages(String chatId);
+  Future<MessageDTO?> sendMessage(String chatId, String message);
+  Future<void> createPrivateChat(String friendId);
+  Future<void> createGroupChat(
+      String groupName, String groupPhotoURL, List<String> participants);
+  Future<void> deleteChat(String chatId);
+}
