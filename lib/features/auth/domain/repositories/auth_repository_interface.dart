d12b706c01f632d236108695b732e7a89bbd1512
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-02-09 21:05:03 -0300

    created AuthRepositoryInterface

---
+++ lib/features/auth/domain/repositories/auth_repository_interface.dart
@@ -0,0 +1,6 @@
+abstract interface class AuthRepositoryInterface {
+  Future<bool> loginWithGoogle();
+  Future<bool> loginWithEmailAndPassword(String email, String password);
+  Future<bool> registerWithEmailAndPassword(String email, String password);
+  Future<bool> logout();
+}
