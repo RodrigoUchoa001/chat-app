62a1042480fb35f74064c9fc829fea0141241bc7
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-02-10 12:07:26 -0300

    added to the FriendsRepositoryInterface functions to accept and reject friend request

--- lib/features/friends/domain/friends_repository_interface.dart
+++ lib/features/friends/domain/friends_repository_interface.dart
@@ -1,6 +1,8 @@
 abstract interface class FriendsRepositoryInterface {
   Future<void> sendFriendRequest(String friendId);
   Future<void> removeFriend(String friendId);
+  Future<void> acceptFriendRequest(String friendId);
+  Future<void> rejectFriendRequest(String friendId);
   Stream<List<String>?> getFriends();
   Stream<List<String>?> getFriendsRequests();
 }
