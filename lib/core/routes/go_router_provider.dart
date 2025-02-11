87097cd4743f032298140dd60ea506de130679cd
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-02-11 09:55:20 -0300

    added login screen to go router provider

--- lib/core/routes/go_router_provider.dart
+++ lib/core/routes/go_router_provider.dart
@@ -1,16 +1,22 @@
 import 'package:chatapp/core/routes/route_names.dart';
+import 'package:chatapp/features/auth/presentation/login_screen.dart';
 import 'package:chatapp/features/onboarding/presentation/onboarding_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 final goRouterProvider = Provider<GoRouter>((ref) {
   return GoRouter(
-    initialLocation: '/onboarding',
+    initialLocation: '/login',
     routes: [
       GoRoute(
         path: '/onboarding',
         name: onBoardingRoute,
         builder: (context, state) => const OnboardingScreen(),
+      ),
+      GoRoute(
+        path: '/login',
+        name: loginRoute,
+        builder: (context, state) => const LoginScreen(),
       ),
     ],
   );
