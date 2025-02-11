77f774e858af67d878ba152268b0b4d1afde7db8
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-02-11 08:54:47 -0300

    created standard button in core folder

--- lib/core/widgets/chat_text_button.dart
+++ lib/core/widgets/chat_text_button.dart
@@ -2,24 +2,33 @@ import 'package:chatapp/gen/fonts.gen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class OnBoardSignUpButton extends ConsumerWidget {
-  const OnBoardSignUpButton({super.key});
+class ChatTextButton extends ConsumerWidget {
+  final Function onTap;
+  final String text;
+  final Color? buttonColor;
+  final Color? textColor;
+  const ChatTextButton(
+      {required this.onTap,
+      required this.text,
+      this.buttonColor,
+      this.textColor,
+      super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return TextButton(
       onPressed: () {},
       style: TextButton.styleFrom(
-        backgroundColor: Colors.white,
+        backgroundColor: buttonColor ?? Colors.white,
         minimumSize: const Size.fromHeight(48),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(16),
         ),
       ),
-      child: const Text(
-        "Sign up with email",
+      child: Text(
+        text,
         style: TextStyle(
-          color: Colors.black,
+          color: textColor ?? Color(0xFF000E08),
           fontFamily: FontFamily.caros,
           fontWeight: FontWeight.normal,
         ),
