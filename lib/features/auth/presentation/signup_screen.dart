2581b85fb3af267da322b37a2c0e257608bd3b82
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-02-11 23:30:02 -0300

    signup screen now has validators on text fields

--- lib/features/auth/presentation/signup_screen.dart
+++ lib/features/auth/presentation/signup_screen.dart
@@ -5,6 +5,7 @@ import 'package:chatapp/features/auth/presentation/widgets/auth_text_field.dart'
 import 'package:chatapp/features/auth/presentation/widgets/auth_title.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:email_validator/email_validator.dart';
 
 class SignupScreen extends ConsumerStatefulWidget {
   const SignupScreen({super.key});
@@ -19,6 +20,31 @@ class _SignupScreenState extends ConsumerState<SignupScreen> {
   final passwordController = TextEditingController();
   final confirmPasswordController = TextEditingController();
 
+  final _formKey = GlobalKey<FormState>();
+
+  String? _textValidator(String? value) {
+    if (value == null || value.trim().isEmpty) {
+      return 'This field cannot be empty.';
+    }
+    return null;
+  }
+
+  String? _emailValidator(String? value) {
+    if (value == null || value.trim().isEmpty) {
+      return 'This field cannot be empty.';
+    }
+    if (!EmailValidator.validate(value)) {
+      return 'Invalid email address.';
+    }
+    return null;
+  }
+
+  void _validate() {
+    if (_formKey.currentState!.validate()) {
+      // TODO: Implement signup
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,41 +55,48 @@ class _SignupScreenState extends ConsumerState<SignupScreen> {
       ),
       body: SafeArea(
         minimum: EdgeInsets.symmetric(horizontal: 30),
-        child: ListView(
-          children: [
-            AuthTitle(
-              title1: 'Sign up',
-              title2: 'with Email',
-              containerWidth: 68.6,
-            ),
-            const SizedBox(height: 17),
-            AuthSubtitle(
-              subtitle:
-                  'Get chatting with friends and family today by signing up for our chat app!',
-            ),
-            const SizedBox(height: 60),
-            AuthTextField(
-              controller: nameController,
-              labelText: 'Your name',
-            ),
-            const SizedBox(height: 30),
-            AuthTextField(
-              controller: emailController,
-              labelText: 'Your email',
-            ),
-            const SizedBox(height: 30),
-            AuthTextField(
-              controller: passwordController,
-              labelText: 'Password',
-              obscureText: true,
-            ),
-            const SizedBox(height: 30),
-            AuthTextField(
-              controller: confirmPasswordController,
-              labelText: 'Confirm Password',
-              obscureText: true,
-            ),
-          ],
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              AuthTitle(
+                title1: 'Sign up',
+                title2: 'with Email',
+                containerWidth: 68.6,
+              ),
+              const SizedBox(height: 17),
+              AuthSubtitle(
+                subtitle:
+                    'Get chatting with friends and family today by signing up for our chat app!',
+              ),
+              const SizedBox(height: 60),
+              AuthTextField(
+                controller: nameController,
+                labelText: 'Your name',
+                validator: _textValidator,
+              ),
+              const SizedBox(height: 30),
+              AuthTextField(
+                controller: emailController,
+                labelText: 'Your email',
+                validator: _emailValidator,
+              ),
+              const SizedBox(height: 30),
+              AuthTextField(
+                controller: passwordController,
+                labelText: 'Password',
+                validator: _textValidator,
+                obscureText: true,
+              ),
+              const SizedBox(height: 30),
+              AuthTextField(
+                controller: confirmPasswordController,
+                labelText: 'Confirm Password',
+                validator: _textValidator,
+                obscureText: true,
+              ),
+            ],
+          ),
         ),
       ),
       bottomSheet: Container(
@@ -74,7 +107,7 @@ class _SignupScreenState extends ConsumerState<SignupScreen> {
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 24),
               child: ChatTextButton(
-                onTap: () {},
+                onTap: () => _validate(),
                 text: 'Create a account',
                 buttonColor: Color(0xFF24786D),
                 textColor: Colors.white,
