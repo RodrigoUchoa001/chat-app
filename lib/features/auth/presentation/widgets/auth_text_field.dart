0b0dd8431285467c576716ae0ef9267f103eaab3
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-02-11 23:10:13 -0300

    altered auth text field to handle validators

--- lib/features/auth/presentation/widgets/auth_text_field.dart
+++ lib/features/auth/presentation/widgets/auth_text_field.dart
@@ -1,45 +1,54 @@
 import 'package:chatapp/gen/fonts.gen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AuthTextField extends ConsumerWidget {
+class AuthTextField extends StatelessWidget {
   final TextEditingController controller;
-  bool obscureText;
+  final bool obscureText;
   final String labelText;
+  final String? Function(String?)? validator;
+
   AuthTextField({
     super.key,
     required this.controller,
-    this.obscureText = false,
     required this.labelText,
+    this.obscureText = false,
+    required this.validator,
   });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return TextField(
-      controller: controller,
-      obscureText: obscureText,
-      cursorColor: Color(0xFF5EBAAE),
-      decoration: InputDecoration(
-        labelText: labelText,
-        labelStyle: TextStyle(
-          color: Color(0xFF5EBAAE),
-          fontFamily: FontFamily.circular,
-          fontSize: 14,
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16),
+      child: TextFormField(
+        controller: controller,
+        obscureText: obscureText,
+        validator: validator,
+        cursorColor: Color(0xFF5EBAAE),
+        decoration: InputDecoration(
+          labelText: labelText,
+          labelStyle: TextStyle(
+            color: Color(0xFF5EBAAE),
+            fontSize: 14,
+          ),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Color(0xFF595E5C)),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Color(0xFF5EBAAE)),
+          ),
+          border: UnderlineInputBorder(
+            borderSide: BorderSide(color: Color(0xFF595E5C)),
+          ),
+          errorStyle: TextStyle(
+            fontFamily: FontFamily.circular,
+            fontSize: 12,
+            color: Color(0xFFFF2D1B),
+          ),
         ),
-        enabledBorder: UnderlineInputBorder(
-          borderSide: BorderSide(color: Color(0xFF595E5C)),
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 16,
         ),
-        focusedBorder: UnderlineInputBorder(
-          borderSide: BorderSide(color: Color(0xFF5EBAAE)),
-        ),
-        border: UnderlineInputBorder(
-          borderSide: BorderSide(color: Color(0xFF595E5C)),
-        ),
-      ),
-      style: TextStyle(
-        color: Colors.white,
-        fontFamily: FontFamily.caros,
-        fontSize: 16,
       ),
     );
   }
