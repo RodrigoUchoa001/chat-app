4cb668f62fb8b2e806a49878fdbd0ec5d7ab4cb4
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-02-12 00:22:16 -0300

    added name field to registerWithEmailAndPassword onAuthRepositoryInterface

--- lib/features/auth/domain/repositories/auth_repository_interface.dart
+++ lib/features/auth/domain/repositories/auth_repository_interface.dart
@@ -1,6 +1,7 @@
 abstract interface class AuthRepositoryInterface {
   Future<bool> loginWithGoogle();
   Future<bool> loginWithEmailAndPassword(String email, String password);
-  Future<bool> registerWithEmailAndPassword(String email, String password);
+  Future<bool> registerWithEmailAndPassword(
+      String name, String email, String password);
   Future<void> logout();
 }
