6fe5e9af7f95ca2bd988034cd514f20f23e7a253
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-02-12 23:55:41 -0300

    added all screens to go router provider

--- lib/core/routes/go_router_provider.dart
+++ lib/core/routes/go_router_provider.dart
@@ -2,6 +2,8 @@ import 'package:chatapp/core/routes/route_names.dart';
 import 'package:chatapp/features/auth/presentation/login_screen.dart';
 import 'package:chatapp/features/auth/presentation/signup_screen.dart';
 import 'package:chatapp/features/auth/presentation/onboarding_screen.dart';
+import 'package:chatapp/features/chat/presentation/chat_screen.dart';
+import 'package:chatapp/features/home/presentation/home_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
@@ -23,6 +25,27 @@ final goRouterProvider = Provider<GoRouter>((ref) {
         path: '/signup',
         name: signupRoute,
         builder: (context, state) => const SignupScreen(),
+      ),
+      // home
+      GoRoute(
+        path: '/home',
+        name: homeRoute,
+        builder: (context, state) => const HomeScreen(),
+      ),
+      GoRoute(
+        path: '/chat',
+        name: chatRoute,
+        builder: (context, state) => const ChatScreen(),
+      ),
+      GoRoute(
+        path: '/friends',
+        name: friendsRoute,
+        builder: (context, state) => const ChatScreen(),
+      ),
+      GoRoute(
+        path: '/settings',
+        name: settingsRoute,
+        builder: (context, state) => const ChatScreen(),
       ),
     ],
   );
