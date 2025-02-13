7e6f9d32716649f2ecfbf3412b92304412beb853
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-02-12 21:35:19 -0300

    added new error messages to the firebase error handler

--- lib/core/errors/firebase_error_handler.dart
+++ lib/core/errors/firebase_error_handler.dart
@@ -9,6 +9,16 @@ class FirebaseErrorHandler {
         return 'The email address is not valid.';
       case 'weak-password':
         return 'The password provided is too weak.';
+      case 'wrong-password':
+        return 'The password is invalid for the given email.';
+      case 'invalid-credential':
+        return 'The supplied auth credential is malformed or has expired.';
+      case 'user-not-found':
+        return 'No user found with the given email.';
+      case 'operation-not-allowed':
+        return 'The operation is not allowed.';
+      case 'network-request-failed':
+        return 'A network error has occurred.';
       default:
         return 'Unknown error: ${e.message}';
     }
