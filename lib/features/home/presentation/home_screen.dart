2ca1745d5eec43ce13fc7f65c5d09657346e4d98
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-02-14 20:03:13 -0300

    added safearea to homescreen

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -19,43 +19,46 @@ class HomeScreen extends ConsumerWidget {
       const ChatScreen(),
     ];
 
-    return Scaffold(
-      body: pages[currentIndex],
-      bottomNavigationBar: SizedBox(
-        height: 90,
-        child: BottomNavigationBar(
-          backgroundColor: const Color(0xFF121414),
-          selectedItemColor: Colors.white,
-          unselectedItemColor: const Color(0xFF797C7B),
-          selectedLabelStyle: const TextStyle(
-            fontFamily: FontFamily.caros,
-            fontSize: 16,
-            color: Colors.white,
+    return SafeArea(
+      child: Scaffold(
+        body: pages[currentIndex],
+        bottomNavigationBar: SizedBox(
+          height: 90,
+          child: BottomNavigationBar(
+            backgroundColor: const Color(0xFF121414),
+            selectedItemColor: Colors.white,
+            unselectedItemColor: const Color(0xFF797C7B),
+            selectedLabelStyle: const TextStyle(
+              fontFamily: FontFamily.caros,
+              fontSize: 16,
+              color: Colors.white,
+            ),
+            unselectedLabelStyle: const TextStyle(
+              fontFamily: FontFamily.caros,
+              fontSize: 16,
+              color: Color(0xFF797C7B),
+            ),
+            showSelectedLabels: true,
+            currentIndex: currentIndex,
+            onTap: (index) =>
+                ref.read(bottomNavIndexProvider.notifier).state = index,
+            items: [
+              BottomNavigationBarItem(
+                icon:
+                    _buildSvgIcon(Assets.icons.message.path, currentIndex == 0),
+                label: 'Chats',
+              ),
+              BottomNavigationBarItem(
+                icon: _buildSvgIcon(Assets.icons.user.path, currentIndex == 1),
+                label: 'Friends',
+              ),
+              BottomNavigationBarItem(
+                icon: _buildSvgIcon(
+                    Assets.icons.settings.path, currentIndex == 2),
+                label: 'Settings',
+              ),
+            ],
           ),
-          unselectedLabelStyle: const TextStyle(
-            fontFamily: FontFamily.caros,
-            fontSize: 16,
-            color: Color(0xFF797C7B),
-          ),
-          showSelectedLabels: true,
-          currentIndex: currentIndex,
-          onTap: (index) =>
-              ref.read(bottomNavIndexProvider.notifier).state = index,
-          items: [
-            BottomNavigationBarItem(
-              icon: _buildSvgIcon(Assets.icons.message.path, currentIndex == 0),
-              label: 'Chats',
-            ),
-            BottomNavigationBarItem(
-              icon: _buildSvgIcon(Assets.icons.user.path, currentIndex == 1),
-              label: 'Friends',
-            ),
-            BottomNavigationBarItem(
-              icon:
-                  _buildSvgIcon(Assets.icons.settings.path, currentIndex == 2),
-              label: 'Settings',
-            ),
-          ],
         ),
       ),
     );
