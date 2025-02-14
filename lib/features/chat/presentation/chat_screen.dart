96ef12d5ef920c899c396f8ca018a42f01229f09
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-02-14 20:44:39 -0300

    added top of chat screen, with title and status row

--- lib/features/chat/presentation/chat_screen.dart
+++ lib/features/chat/presentation/chat_screen.dart
@@ -1,5 +1,8 @@
+import 'package:chatapp/gen/assets.gen.dart';
+import 'package:chatapp/gen/fonts.gen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
 
 class ChatScreen extends ConsumerStatefulWidget {
   const ChatScreen({super.key});
@@ -11,8 +14,172 @@ class ChatScreen extends ConsumerStatefulWidget {
 class _ChatScreenState extends ConsumerState<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: const Text('Chat'),
+    return SafeArea(
+      child: Scaffold(
+        // NEXT TODO: Implement chat screen
+        body: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                color: Color(0xFF24786D),
+              ),
+              child: Column(
+                children: [
+                  SizedBox(height: 17),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          width: 44,
+                          height: 44,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Color(0xFF4B9289),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          child: SvgPicture.asset(
+                            Assets.icons.search.path,
+                            fit: BoxFit.scaleDown,
+                            height: 18.33,
+                            width: 18.33,
+                          ),
+                        ),
+                        Text(
+                          'Home',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: FontFamily.caros,
+                            fontSize: 20,
+                          ),
+                        ),
+                        CircleAvatar(
+                          backgroundImage: NetworkImage(
+                            scale: 44,
+                            'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 40),
+                  SizedBox(
+                    height: 100,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 13),
+                          child: Column(
+                            children: [
+                              Stack(
+                                alignment: Alignment.bottomRight,
+                                children: [
+                                  Container(
+                                    height: 58,
+                                    width: 58,
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                        color: Color(0xFF4B9289),
+                                        width: 1,
+                                      ),
+                                      borderRadius: BorderRadius.circular(50),
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(6),
+                                      child: CircleAvatar(
+                                        backgroundImage: NetworkImage(
+                                          // scale: 52,
+                                          'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    height: 16,
+                                    width: 16,
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                        color: Color(0xFF24786D),
+                                        width: 1,
+                                      ),
+                                      borderRadius: BorderRadius.circular(50),
+                                      color: Colors.white,
+                                    ),
+                                    child: Icon(Icons.add,
+                                        size: 10, color: Color(0xFF24786D)),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 10),
+                              const Text(
+                                'My Status',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: FontFamily.caros,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        ListView.builder(
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 8,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 13),
+                              child: Column(
+                                children: [
+                                  Container(
+                                    height: 58,
+                                    width: 58,
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                        color: Color(0xFF4B9289),
+                                        width: 1,
+                                      ),
+                                      borderRadius: BorderRadius.circular(50),
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(6),
+                                      child: CircleAvatar(
+                                        backgroundImage: NetworkImage(
+                                          // scale: 52,
+                                          'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  const Text(
+                                    'John Doe',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: FontFamily.caros,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
