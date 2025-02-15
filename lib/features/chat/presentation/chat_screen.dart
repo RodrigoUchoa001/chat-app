eceafb8d7a208021591a54204499829adde80b64
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-02-14 21:19:16 -0300

    chat screen now using separated file of chat list

--- lib/features/chat/presentation/chat_screen.dart
+++ lib/features/chat/presentation/chat_screen.dart
@@ -1,6 +1,6 @@
 import 'package:chatapp/features/chat/presentation/widgets/chat_app_bar.dart';
+import 'package:chatapp/features/chat/presentation/widgets/chat_list.dart';
 import 'package:chatapp/features/chat/presentation/widgets/chat_status_row.dart';
-import 'package:chatapp/gen/fonts.gen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -18,7 +18,7 @@ class _ChatScreenState extends ConsumerState<ChatScreen> {
 
     return SafeArea(
       child: Scaffold(
-        // NEXT TODO: Implement chat screen
+        // NEXT TODO: Implement chat screen now using firebase
         body: Stack(
           alignment: Alignment.bottomCenter,
           children: [
@@ -45,103 +45,7 @@ class _ChatScreenState extends ConsumerState<ChatScreen> {
                   topRight: Radius.circular(50),
                 ),
               ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 24, right: 24, top: 26),
-                child: ListView.builder(
-                  itemCount: 10,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 15),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              Stack(
-                                alignment: Alignment.bottomRight,
-                                children: [
-                                  CircleAvatar(
-                                    radius: 26,
-                                    backgroundImage: NetworkImage(
-                                      'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(4),
-                                    child: Container(
-                                      width: 8,
-                                      height: 8,
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(50),
-                                        color: Color(0xFF0FE16D),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              const SizedBox(width: 12),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  const Text(
-                                    'Alex Linderson',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20,
-                                      fontFamily: FontFamily.caros,
-                                    ),
-                                  ),
-                                  const SizedBox(height: 6),
-                                  const Text(
-                                    'How are you today?',
-                                    style: TextStyle(
-                                      color: Color(0xFF797C7B),
-                                      fontSize: 12,
-                                      fontFamily: FontFamily.circular,
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ],
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              const Text(
-                                '2 min ago',
-                                style: TextStyle(
-                                  color: Color(0xFF797C7B),
-                                  fontSize: 12,
-                                  fontFamily: FontFamily.circular,
-                                ),
-                              ),
-                              const SizedBox(height: 7),
-                              Container(
-                                width: 21.81,
-                                height: 21.81,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(50),
-                                  color: Color(0xFFF04A4C),
-                                ),
-                                child: Center(
-                                  child: const Text(
-                                    '3',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 12,
-                                      fontFamily: FontFamily.circular,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              ),
+              child: ChatList(),
             ),
           ],
         ),
