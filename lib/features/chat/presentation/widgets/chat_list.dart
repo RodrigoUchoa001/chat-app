495adc8424cf49bafaffc4b124789ffc7e0de3e3
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-02-17 12:24:11 -0300

    added button that will take to chat screen

--- lib/features/chat/presentation/widgets/chat_list.dart
+++ lib/features/chat/presentation/widgets/chat_list.dart
@@ -21,102 +21,105 @@ class ChatList extends ConsumerWidget {
           itemBuilder: (context, index) {
             final chat = chats[index];
 
-            return Padding(
-              padding: const EdgeInsets.symmetric(vertical: 15),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Stack(
-                        alignment: Alignment.bottomRight,
-                        children: [
-                          CircleAvatar(
-                            radius: 26,
-                            backgroundImage: NetworkImage(
-                              chat.groupPhotoURL ?? '',
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(4),
-                            child: Container(
-                              width: 8,
-                              height: 8,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(50),
-                                color: Color(0xFF0FE16D),
+            return TextButton(
+              onPressed: () {},
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Stack(
+                          alignment: Alignment.bottomRight,
+                          children: [
+                            CircleAvatar(
+                              radius: 26,
+                              backgroundImage: NetworkImage(
+                                chat.groupPhotoURL ?? '',
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(width: 12),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            chat.groupName ?? 'No name',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontFamily: FontFamily.caros,
+                            Padding(
+                              padding: const EdgeInsets.all(4),
+                              child: Container(
+                                width: 8,
+                                height: 8,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(50),
+                                  color: Color(0xFF0FE16D),
+                                ),
+                              ),
                             ),
-                          ),
-                          const SizedBox(height: 6),
-                          Text(
-                            chat.lastMessage!.text ?? 'No message',
-                            style: TextStyle(
-                              color: Color(0xFF797C7B),
-                              fontSize: 12,
-                              fontFamily: FontFamily.circular,
-                            ),
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text(
-                        calculateTimeSinceLastMessage(
-                            chat.lastMessage!.timestamp!),
-                        style: TextStyle(
-                          color: Color(0xFF797C7B),
-                          fontSize: 12,
-                          fontFamily: FontFamily.circular,
+                          ],
                         ),
-                      ),
-                      const SizedBox(height: 7),
-                      unseenMessagesCount.when(
-                        data: (count) {
-                          return count == 0
-                              ? Container()
-                              : Container(
-                                  width: 21.81,
-                                  height: 21.81,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(50),
-                                    color: Color(0xFFF04A4C),
-                                  ),
-                                  child: Center(
-                                    child: Text(
-                                      count.toString(),
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 12,
-                                        fontFamily: FontFamily.circular,
+                        const SizedBox(width: 12),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              chat.groupName ?? 'No name',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontFamily: FontFamily.caros,
+                              ),
+                            ),
+                            const SizedBox(height: 6),
+                            Text(
+                              chat.lastMessage!.text ?? 'No message',
+                              style: TextStyle(
+                                color: Color(0xFF797C7B),
+                                fontSize: 12,
+                                fontFamily: FontFamily.circular,
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          calculateTimeSinceLastMessage(
+                              chat.lastMessage!.timestamp!),
+                          style: TextStyle(
+                            color: Color(0xFF797C7B),
+                            fontSize: 12,
+                            fontFamily: FontFamily.circular,
+                          ),
+                        ),
+                        const SizedBox(height: 7),
+                        unseenMessagesCount.when(
+                          data: (count) {
+                            return count == 0
+                                ? Container()
+                                : Container(
+                                    width: 21.81,
+                                    height: 21.81,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(50),
+                                      color: Color(0xFFF04A4C),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        count.toString(),
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 12,
+                                          fontFamily: FontFamily.circular,
+                                        ),
                                       ),
                                     ),
-                                  ),
-                                );
-                        },
-                        error: (error, stackTrace) => Container(),
-                        loading: () => Container(),
-                      )
-                    ],
-                  ),
-                ],
+                                  );
+                          },
+                          error: (error, stackTrace) => Container(),
+                          loading: () => Container(),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
               ),
             );
           },
