379187ce3a5053369272001ba39496535d909ae0
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-02-17 11:52:14 -0300

    using mock list chat for test

--- lib/features/chat/presentation/widgets/chat_list.dart
+++ lib/features/chat/presentation/widgets/chat_list.dart
@@ -1,3 +1,5 @@
+import 'package:chatapp/features/chat/data/repositories/fake_chat_repository.dart';
+import 'package:chatapp/features/chat/presentation/utils/calculate_time_since_last_message.dart';
 import 'package:chatapp/gen/fonts.gen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -7,101 +9,110 @@ class ChatList extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final fakeChats = ref.watch(mockUserChatsProvider);
+
     return Padding(
       padding: const EdgeInsets.only(left: 24, right: 24, top: 26),
-      child: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 15),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Stack(
-                      alignment: Alignment.bottomRight,
-                      children: [
-                        CircleAvatar(
-                          radius: 26,
-                          backgroundImage: NetworkImage(
-                            'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
+      child: fakeChats.when(
+        data: (chats) => ListView.builder(
+          itemCount: chats!.length,
+          itemBuilder: (context, index) {
+            final chat = chats[index];
+
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Stack(
+                        alignment: Alignment.bottomRight,
+                        children: [
+                          CircleAvatar(
+                            radius: 26,
+                            backgroundImage: NetworkImage(
+                              chat.groupPhotoURL ?? '',
+                            ),
                           ),
+                          Padding(
+                            padding: const EdgeInsets.all(4),
+                            child: Container(
+                              width: 8,
+                              height: 8,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                color: Color(0xFF0FE16D),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(width: 12),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            chat.groupName ?? 'No name',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontFamily: FontFamily.caros,
+                            ),
+                          ),
+                          const SizedBox(height: 6),
+                          Text(
+                            chat.lastMessage!.text ?? 'No message',
+                            style: TextStyle(
+                              color: Color(0xFF797C7B),
+                              fontSize: 12,
+                              fontFamily: FontFamily.circular,
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        calculateTimeSinceLastMessage(
+                            chat.lastMessage!.timestamp!),
+                        style: TextStyle(
+                          color: Color(0xFF797C7B),
+                          fontSize: 12,
+                          fontFamily: FontFamily.circular,
                         ),
-                        Padding(
-                          padding: const EdgeInsets.all(4),
-                          child: Container(
-                            width: 8,
-                            height: 8,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(50),
-                              color: Color(0xFF0FE16D),
+                      ),
+                      const SizedBox(height: 7),
+                      Container(
+                        width: 21.81,
+                        height: 21.81,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: Color(0xFFF04A4C),
+                        ),
+                        child: Center(
+                          child: const Text(
+                            '3',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              fontFamily: FontFamily.circular,
                             ),
                           ),
                         ),
-                      ],
-                    ),
-                    const SizedBox(width: 12),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          'Alex Linderson',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontFamily: FontFamily.caros,
-                          ),
-                        ),
-                        const SizedBox(height: 6),
-                        const Text(
-                          'How are you today?',
-                          style: TextStyle(
-                            color: Color(0xFF797C7B),
-                            fontSize: 12,
-                            fontFamily: FontFamily.circular,
-                          ),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    const Text(
-                      '2 min ago',
-                      style: TextStyle(
-                        color: Color(0xFF797C7B),
-                        fontSize: 12,
-                        fontFamily: FontFamily.circular,
                       ),
-                    ),
-                    const SizedBox(height: 7),
-                    Container(
-                      width: 21.81,
-                      height: 21.81,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        color: Color(0xFFF04A4C),
-                      ),
-                      child: Center(
-                        child: const Text(
-                          '3',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 12,
-                            fontFamily: FontFamily.circular,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          );
-        },
+                    ],
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+        error: (error, stackTrace) => Center(child: Text(error.toString())),
+        loading: () => const Center(child: CircularProgressIndicator()),
       ),
     );
   }
