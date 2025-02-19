8818dd6811e739ad8f6f7b4cb84be2359169812b
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-02-19 10:08:13 -0300

    now the chat list widget gets the chats from firebase

--- lib/features/chat/presentation/widgets/chat_list.dart
+++ lib/features/chat/presentation/widgets/chat_list.dart
@@ -1,4 +1,5 @@
-import 'package:chatapp/features/chat/data/repositories/fake_chat_repository.dart';
+import 'package:chatapp/core/providers/firebase_auth_providers.dart';
+import 'package:chatapp/features/chat/data/repositories/chat_repository.dart';
 import 'package:chatapp/features/chat/presentation/utils/calculate_time_since_last_message.dart';
 import 'package:chatapp/features/chat/presentation/widgets/chat_profile_pic.dart';
 import 'package:chatapp/gen/fonts.gen.dart';
@@ -11,107 +12,133 @@ class ChatList extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final fakeChats = ref.watch(mockUserChatsProvider);
-
-    final unseenMessagesCount = ref.watch(unreadMessagesProvider('user1'));
+    final chats = ref.watch(chatRepositoryProvider);
+    final user = ref.watch(currentUserProvider).asData?.value;
+    final unseenMessagesCount = ref.watch(unreadMessagesProvider(user!.uid));
 
     return Padding(
       padding: const EdgeInsets.only(left: 24, right: 24, top: 26),
-      child: fakeChats.when(
-        data: (chats) => ListView.builder(
-          itemCount: chats!.length,
-          itemBuilder: (context, index) {
-            final chat = chats[index];
+      child: StreamBuilder(
+        stream: chats.getChats(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
 
-            return TextButton(
-              onPressed: () {
-                context.push('/chat/${chat.id}');
-              },
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 15),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        ChatProfilePic(
-                          groupPhotoURL: chat.groupPhotoURL ?? '',
-                          isOnline: true,
-                        ),
-                        const SizedBox(width: 12),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              chat.groupName ?? 'No name',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontFamily: FontFamily.caros,
-                              ),
-                            ),
-                            const SizedBox(height: 6),
-                            Text(
-                              chat.lastMessage!.text ?? 'No message',
-                              style: TextStyle(
-                                color: Color(0xFF797C7B),
-                                fontSize: 12,
-                                fontFamily: FontFamily.circular,
-                              ),
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Text(
-                          calculateTimeSinceLastMessage(
-                              chat.lastMessage!.timestamp!),
-                          style: TextStyle(
-                            color: Color(0xFF797C7B),
-                            fontSize: 12,
-                            fontFamily: FontFamily.circular,
-                          ),
-                        ),
-                        const SizedBox(height: 7),
-                        unseenMessagesCount.when(
-                          data: (count) {
-                            return count == 0
-                                ? Container()
-                                : Container(
-                                    width: 21.81,
-                                    height: 21.81,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(50),
-                                      color: Color(0xFFF04A4C),
-                                    ),
-                                    child: Center(
-                                      child: Text(
-                                        count.toString(),
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 12,
-                                          fontFamily: FontFamily.circular,
-                                        ),
-                                      ),
-                                    ),
-                                  );
-                          },
-                          error: (error, stackTrace) => Container(),
-                          loading: () => Container(),
-                        )
-                      ],
-                    ),
-                  ],
+          final data = snapshot.data;
+          if (data!.isEmpty) {
+            return Center(
+              child: const Text(
+                'No chats yet!',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontFamily: FontFamily.circular,
                 ),
               ),
             );
-          },
-        ),
-        error: (error, stackTrace) => Center(child: Text(error.toString())),
-        loading: () => const Center(child: CircularProgressIndicator()),
+          }
+
+          return ListView.builder(
+            itemCount: data.length,
+            itemBuilder: (context, index) {
+              final chat = data[index];
+
+              return TextButton(
+                onPressed: () {
+                  context.push('/chat/${chat.id}');
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          ChatProfilePic(
+                            chatPhotoURL: chat.type == 'group'
+                                ? chat.groupPhotoURL ?? ''
+                                : user.photoURL ?? '',
+                            isOnline: true,
+                          ),
+                          const SizedBox(width: 12),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                chat.type == 'group'
+                                    ? chat.groupName ?? 'No name'
+                                    : 'private friend name',
+                                // NEXT TODO: get friend name from firebase when is a private chat
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontFamily: FontFamily.caros,
+                                ),
+                              ),
+                              const SizedBox(height: 6),
+                              Text(
+                                chat.lastMessage!.text ?? 'No message',
+                                style: TextStyle(
+                                  color: Color(0xFF797C7B),
+                                  fontSize: 12,
+                                  fontFamily: FontFamily.circular,
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            calculateTimeSinceLastMessage(
+                                chat.lastMessage!.timestamp!),
+                            style: TextStyle(
+                              color: Color(0xFF797C7B),
+                              fontSize: 12,
+                              fontFamily: FontFamily.circular,
+                            ),
+                          ),
+                          const SizedBox(height: 7),
+                          unseenMessagesCount.when(
+                            data: (count) {
+                              return count == 0
+                                  ? Container()
+                                  : Container(
+                                      width: 21.81,
+                                      height: 21.81,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(50),
+                                        color: Color(0xFFF04A4C),
+                                      ),
+                                      child: Center(
+                                        child: Text(
+                                          count.toString(),
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 12,
+                                            fontFamily: FontFamily.circular,
+                                          ),
+                                        ),
+                                      ),
+                                    );
+                            },
+                            error: (error, stackTrace) => SizedBox(),
+                            loading: () => SizedBox(),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
