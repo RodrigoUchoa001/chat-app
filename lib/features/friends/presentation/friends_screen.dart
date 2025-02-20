9e44ad6baee2fc76319159b9f8d41f51fe83c337
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-02-20 20:43:00 -0300

    started friends screen

---
+++ lib/features/friends/presentation/friends_screen.dart
@@ -0,0 +1,66 @@
+import 'package:chatapp/core/widgets/app_bar_widget.dart';
+import 'package:chatapp/core/widgets/home_content_background_widget.dart';
+import 'package:chatapp/gen/assets.gen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
+
+class FriendsScreen extends ConsumerStatefulWidget {
+  const FriendsScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _FriendsScreenState();
+}
+
+class _FriendsScreenState extends ConsumerState<FriendsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                color: Color(0xFF24786D),
+              ),
+              child: Column(
+                children: [
+                  SizedBox(height: 17),
+                  AppBarWidget(
+                    leftButton: IconButton(
+                      onPressed: () {},
+                      icon: SvgPicture.asset(
+                        Assets.icons.search.path,
+                        fit: BoxFit.scaleDown,
+                        height: 18.33,
+                        width: 18.33,
+                      ),
+                    ),
+                    title: "Friends",
+                    rightButton: IconButton(
+                      onPressed: () {},
+                      icon: SvgPicture.asset(
+                        Assets.icons.userAdd.path,
+                        fit: BoxFit.scaleDown,
+                        height: 24,
+                        width: 24,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                ],
+              ),
+            ),
+            HomeContentBackground(
+              height: screenHeight - 175.33, //CHUTEI ESSE NUMERO
+              child: Container(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
