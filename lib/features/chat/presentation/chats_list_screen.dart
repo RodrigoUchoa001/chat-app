135b37d2a991ca66a5aa943c5226dade1538d46f
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-02-20 20:26:35 -0300

    now using AppBarWidget on chats list screen

--- lib/features/chat/presentation/chats_list_screen.dart
+++ lib/features/chat/presentation/chats_list_screen.dart
@@ -1,8 +1,10 @@
-import 'package:chatapp/features/chat/presentation/widgets/chat_list_app_bar.dart';
+import 'package:chatapp/core/widgets/app_bar_widget.dart';
 import 'package:chatapp/features/chat/presentation/widgets/chat_list.dart';
 import 'package:chatapp/features/chat/presentation/widgets/chat_status_row.dart';
+import 'package:chatapp/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
 
 class ChatsListScreen extends ConsumerStatefulWidget {
   const ChatsListScreen({super.key});
@@ -28,7 +30,24 @@ class _ChatScreenState extends ConsumerState<ChatsListScreen> {
               child: Column(
                 children: [
                   SizedBox(height: 17),
-                  ChatListAppBar(),
+                  AppBarWidget(
+                    leftButton: IconButton(
+                      onPressed: () {},
+                      icon: SvgPicture.asset(
+                        Assets.icons.search.path,
+                        fit: BoxFit.scaleDown,
+                        height: 18.33,
+                        width: 18.33,
+                      ),
+                    ),
+                    title: 'Home',
+                    rightButton: CircleAvatar(
+                      radius: 22,
+                      backgroundImage: NetworkImage(
+                        'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
+                      ),
+                    ),
+                  ),
                   SizedBox(height: 40),
                   ChatStatusRow(),
                 ],
