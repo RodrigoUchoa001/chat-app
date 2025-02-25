dd0d0bfcf8a1b00627a7285a6b44943a19ea3731
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-02-25 09:27:07 -0300

    now the chat screen shows the send message icon if has text in the ChatTextField

--- lib/features/chat/presentation/widgets/chat_input_field.dart
+++ lib/features/chat/presentation/widgets/chat_input_field.dart
@@ -1,3 +1,4 @@
+import 'package:chatapp/features/chat/presentation/providers/show_send_message_icon_provider.dart';
 import 'package:chatapp/features/chat/presentation/widgets/chat_icon_button.dart';
 import 'package:chatapp/features/chat/presentation/widgets/chat_text_field.dart';
 import 'package:chatapp/gen/assets.gen.dart';
@@ -8,8 +9,8 @@ class ChatInputField extends ConsumerWidget {
   const ChatInputField({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final chatTextFieldController = TextEditingController();
+  Widget build(BuildContext context) {
+    final showSendMessageIcon = ref.watch(showSendMessageIconProvider);
 
     return Container(
       height: 90,
@@ -26,19 +27,57 @@ class ChatInputField extends ConsumerWidget {
         padding: const EdgeInsets.symmetric(horizontal: 24),
         child: Row(
           children: [
-            ChatIconButton(iconPath: Assets.icons.clip.path),
+            ChatIconButton(
+              iconPath: Assets.icons.clip.path,
+              onPressed: () {},
+            ),
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: ChatTextField(controller: chatTextFieldController),
+                child: ChatTextField(
+                  onChanged: (text) {
+                    if (text.isNotEmpty) {
+                      ref
+                          .read(showSendMessageIconProvider.notifier)
+                          .update((state) => true);
+                    } else {
+                      ref
+                          .read(showSendMessageIconProvider.notifier)
+                          .update((state) => false);
+                    }
+                  },
+                  controller: _chatTextFieldController,
+                ),
               ),
             ),
-            ChatIconButton(iconPath: Assets.icons.camera.path),
-            SizedBox(width: 16),
-            ChatIconButton(iconPath: Assets.icons.microphone.path),
+            _buildButtons(showSendMessageIcon),
           ],
         ),
       ),
     );
+  }
+
+  Widget _buildButtons(bool showSendMessageIcon) {
+    if (showSendMessageIcon) {
+      return ChatIconButton(
+        iconPath: Assets.icons.send.path,
+        backgroundColor: Color(0xFF20A090),
+        onPressed: () {},
+      );
+    } else {
+      return Row(
+        children: [
+          ChatIconButton(
+            iconPath: Assets.icons.camera.path,
+            onPressed: () {},
+          ),
+          SizedBox(width: 16),
+          ChatIconButton(
+            iconPath: Assets.icons.microphone.path,
+            onPressed: () {},
+          ),
+        ],
+      );
+    }
   }
 }
