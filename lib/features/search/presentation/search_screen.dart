51f26619d2955e431afa09c2277e2d6daf07613d
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-02-27 22:01:36 -0300

    initiated search screen

---
+++ lib/features/search/presentation/search_screen.dart
@@ -0,0 +1,95 @@
+import 'package:chatapp/gen/assets.gen.dart';
+import 'package:chatapp/gen/fonts.gen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
+
+class SearchScreen extends ConsumerStatefulWidget {
+  const SearchScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends ConsumerState<SearchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final searchController = TextEditingController();
+
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xFF121414),
+        appBar: AppBar(
+          backgroundColor: Color(0xFF121414),
+          leading: IconButton(
+            icon: SvgPicture.asset(
+              Assets.icons.backButton.path,
+              fit: BoxFit.scaleDown,
+              width: 18,
+              height: 18,
+            ),
+            onPressed: () {
+              context.pop();
+            },
+          ),
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 17),
+              child: Container(
+                height: 44,
+                decoration: BoxDecoration(
+                  color: Color(0xFF192222),
+                  border: Border(
+                    top: BorderSide(
+                      color: Color(0xFF192222),
+                      width: 1,
+                    ),
+                  ),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: TextField(
+                  controller: searchController,
+                  onChanged: (value) {},
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                    fontFamily: FontFamily.circular,
+                  ),
+                  cursorColor: Colors.white,
+                  autofocus: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 0,
+                        style: BorderStyle.none,
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    prefixIcon: SvgPicture.asset(
+                      Assets.icons.search.path,
+                      fit: BoxFit.scaleDown,
+                      width: 20,
+                      height: 20,
+                    ),
+                    filled: true,
+                    fillColor: Color(0xFF192222),
+                    hintText: 'Type to search',
+                    hintStyle: TextStyle(
+                      color: Color(0xFF797C7B),
+                      fontSize: 12,
+                      fontFamily: FontFamily.circular,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            // TODO: add search results
+          ],
+        ),
+      ),
+    );
+  }
+}
