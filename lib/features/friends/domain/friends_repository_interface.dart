236ab42daf3b80b6b9601c49132b92f1ef2ec1d8
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-02-28 22:02:59 -0300

    added searchFriends to FriendsRepositoryInterface

--- lib/features/friends/domain/friends_repository_interface.dart
+++ lib/features/friends/domain/friends_repository_interface.dart
@@ -1,8 +1,11 @@
+import 'package:chatapp/features/auth/data/dto/user_dto.dart';
+
 abstract interface class FriendsRepositoryInterface {
   Future<void> sendFriendRequest(String friendId);
   Future<void> removeFriend(String friendId);
   Future<void> acceptFriendRequest(String friendId);
   Future<void> rejectFriendRequest(String friendId);
   Stream<List<String>?> getFriends();
+  Stream<List<UserDTO>> searchFriends(String query);
   Stream<List<String>?> getFriendsRequests();
 }
