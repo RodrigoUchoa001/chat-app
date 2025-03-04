36e196615529593a7daa6f7329574412c34d08e0
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-03-03 23:25:14 -0300

    created function searchUsers in UserRepositoryInterface

--- lib/features/users/domain/user_repository_interface.dart
+++ lib/features/users/domain/user_repository_interface.dart
@@ -2,4 +2,5 @@ import 'package:chatapp/features/auth/data/dto/user_dto.dart';
 
 abstract interface class UserRepositoryInterface {
   Stream<UserDTO?> getUserDetails(String userId);
+  Stream<List<UserDTO>> searchUsers(String query);
 }
