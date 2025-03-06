edc265670021667dd5e72665489e257fc11b5b37
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-03-05 21:06:27 -0300

    created functions that controls the user's online status by closing or opening the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:chatapp/core/providers/firebase_auth_providers.dart';
 import 'package:chatapp/core/routes/go_router_provider.dart';
+import 'package:chatapp/features/users/data/repositories/user_repository.dart';
 import 'package:chatapp/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -16,16 +18,84 @@ void main() async {
   );
 }
 
-class MainApp extends ConsumerWidget {
+class MainApp extends ConsumerStatefulWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  _MainAppState createState() => _MainAppState();
+}
+
+class _MainAppState extends ConsumerState<MainApp> with WidgetsBindingObserver {
+  String? _userId;
+  bool _isInitialized = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  Future<void> _initializeUser() async {
+    final user = ref.watch(currentUserProvider).asData?.value;
+    if (user != null) {
+      _userId = user.uid;
+      await _setUserOnline();
+    }
+    _isInitialized = true;
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (_userId == null) return;
+
+    if (state == AppLifecycleState.resumed) {
+      _setUserOnline();
+    } else if (state == AppLifecycleState.paused ||
+        state == AppLifecycleState.detached) {
+      _setUserOffline();
+    }
+    super.didChangeAppLifecycleState(state);
+  }
+
+  Future<void> _setUserOnline() async {
+    final userRepo = ref.watch(userRepositoryProvider);
+
+    if (_userId != null) {
+      await userRepo.updateUserOnlineStatus(isOnline: true);
+    }
+  }
+
+  Future<void> _setUserOffline() async {
+    final userRepo = ref.watch(userRepositoryProvider);
+
+    if (_userId != null) {
+      await userRepo.updateUserOnlineStatus(isOnline: false);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final goRouter = ref.watch(goRouterProvider);
 
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: goRouter,
-    );
+    return FutureBuilder(
+        future: _initializeUser(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData || !_isInitialized) {
+            return MaterialApp.router(
+              debugShowCheckedModeBanner: false,
+              routerConfig: goRouter,
+            );
+          }
+          return MaterialApp.router(
+            debugShowCheckedModeBanner: false,
+            routerConfig: goRouter,
+          );
+        });
   }
 }
