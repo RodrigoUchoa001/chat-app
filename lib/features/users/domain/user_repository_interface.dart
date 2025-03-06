813bed228b17eb1609f1e90cfa886867ceb435da
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-03-05 21:04:19 -0300

    created updateUserOnlineStatus on UserRepositoryInterface

--- lib/features/users/domain/user_repository_interface.dart
+++ lib/features/users/domain/user_repository_interface.dart
@@ -3,4 +3,5 @@ import 'package:chatapp/features/auth/data/dto/user_dto.dart';
 abstract interface class UserRepositoryInterface {
   Stream<UserDTO?> getUserDetails(String userId);
   Stream<List<UserDTO>> searchUsers(String query);
+  Future<void> updateUserOnlineStatus({required bool isOnline});
 }
