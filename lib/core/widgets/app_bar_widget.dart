54b50941f076a312d79b2ce8b285810ed484acc5
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-03-07 22:00:08 -0300

    improved AppBarWidget, to be able to receive or not left and right buttons

--- lib/core/widgets/app_bar_widget.dart
+++ lib/core/widgets/app_bar_widget.dart
@@ -3,55 +3,61 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class AppBarWidget extends ConsumerWidget {
-  final Widget leftButton;
+  final Widget? leftButton;
   final String title;
-  final Widget rightButton;
+  final Widget? rightButton;
   const AppBarWidget(
-      {required this.leftButton,
-      required this.title,
-      required this.rightButton,
-      super.key});
+      {this.leftButton, required this.title, this.rightButton, super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            width: 44,
-            height: 44,
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Color(0xFF4B9289),
-                width: 1,
+      child: SizedBox(
+        height: 44,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            if (leftButton != null)
+              Container(
+                width: 44,
+                height: 44,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Color(0xFF4B9289),
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: leftButton,
+              )
+            else
+              const SizedBox(),
+            Text(
+              title,
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: FontFamily.caros,
+                fontSize: 20,
               ),
-              borderRadius: BorderRadius.circular(50),
             ),
-            child: leftButton,
-          ),
-          Text(
-            title,
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: FontFamily.caros,
-              fontSize: 20,
-            ),
-          ),
-          Container(
-            width: 44,
-            height: 44,
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Color(0xFF4B9289),
-                width: 1,
-              ),
-              borderRadius: BorderRadius.circular(50),
-            ),
-            child: rightButton,
-          ),
-        ],
+            if (rightButton != null)
+              Container(
+                width: 44,
+                height: 44,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Color(0xFF4B9289),
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: rightButton,
+              )
+            else
+              const SizedBox(),
+          ],
+        ),
       ),
     );
   }
