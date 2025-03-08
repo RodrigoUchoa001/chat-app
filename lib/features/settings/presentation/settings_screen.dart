a015a68924ae58c532283fa498d9c320550badb4
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-03-07 21:56:41 -0300

    started settings screen

---
+++ lib/features/settings/presentation/settings_screen.dart
@@ -0,0 +1,48 @@
+import 'package:chatapp/core/widgets/app_bar_widget.dart';
+import 'package:chatapp/core/widgets/home_content_background_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class SettingsScreen extends ConsumerStatefulWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends ConsumerState<SettingsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                color: Color(0xFF24786D),
+              ),
+              child: Column(
+                children: [
+                  SizedBox(height: 17),
+                  AppBarWidget(
+                    leftButton: SizedBox(),
+                    title: "Settings",
+                    rightButton: SizedBox(),
+                  ),
+                  SizedBox(height: 30),
+                ],
+              ),
+            ),
+            HomeContentBackground(
+              height: screenHeight - 190, //CHUTEI ESSE NUMERO
+              child: Container(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
