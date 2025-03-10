81efdfd0445313eb48c4695ceb6a7049a5473c7e
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-03-10 20:18:50 -0300

    created functions to change account info

--- lib/features/settings/presentation/account_settings_screen.dart
+++ lib/features/settings/presentation/account_settings_screen.dart
@@ -1,9 +1,13 @@
+import 'package:chatapp/core/providers/firebase_auth_providers.dart';
+import 'package:chatapp/core/widgets/app_bar_widget.dart';
 import 'package:chatapp/features/settings/presentation/widgets/setting_button.dart';
+import 'package:chatapp/features/users/data/repositories/user_repository.dart';
 import 'package:chatapp/gen/assets.gen.dart';
 import 'package:chatapp/gen/fonts.gen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:go_router/go_router.dart';
 
 class AccountSettingsScreen extends ConsumerStatefulWidget {
@@ -17,6 +21,9 @@ class AccountSettingsScreen extends ConsumerStatefulWidget {
 class _AccountSettingsScreenState extends ConsumerState<AccountSettingsScreen> {
   @override
   Widget build(BuildContext context) {
+    final userRepo = ref.watch(userRepositoryProvider);
+    final currentUser = ref.watch(currentUserProvider).asData?.value;
+
     return SafeArea(
       child: Scaffold(
         backgroundColor: Color(0xFF121414),
@@ -29,8 +36,8 @@ class _AccountSettingsScreenState extends ConsumerState<AccountSettingsScreen> {
             onPressed: () => context.pop(),
           ),
           centerTitle: true,
-          title: const Text(
-            'Account Settings',
+          title: Text(
+            "Account Settings",
             style: TextStyle(
               color: Colors.white,
               fontSize: 20,
@@ -40,36 +47,123 @@ class _AccountSettingsScreenState extends ConsumerState<AccountSettingsScreen> {
         ),
         body: Padding(
           padding: const EdgeInsets.symmetric(vertical: 16.0),
-          child: ListView(
-            children: [
-              SettingButton(
-                title: "Change name",
-                subtitle: "subtitle",
-                onTap: () {},
-                trailing: const Icon(
-                  Icons.arrow_forward_ios,
-                  color: Colors.white,
-                ),
-              ),
-              SettingButton(
-                title: "Change name",
-                subtitle: "subtitle",
-                onTap: () {},
-                trailing: const Icon(
-                  Icons.arrow_forward_ios,
-                  color: Colors.white,
-                ),
-              ),
-              SettingButton(
-                title: "Change name",
-                subtitle: "subtitle",
-                onTap: () {},
-                trailing: const Icon(
-                  Icons.arrow_forward_ios,
-                  color: Colors.white,
-                ),
-              ),
-            ],
+          child: StreamBuilder(
+            stream: userRepo.getUserDetails(currentUser!.uid),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(
+                    child: Text('Error: ${snapshot.error}',
+                        style: const TextStyle(color: Colors.red)));
+              }
+
+              final user = snapshot.data!;
+              return ListView(
+                children: [
+                  SettingButton(
+                    title: "Change name",
+                    subtitle: user.name ?? "",
+                    onTap: () async {
+                      final newName = await showDialog(
+                        context: context,
+                        builder: (context) {
+                          String tempName = user.name ?? "";
+                          return AlertDialog(
+                            title: const Text("Change Name"),
+                            content: TextField(
+                              onChanged: (value) {
+                                tempName = value;
+                              },
+                              controller: TextEditingController(text: tempName),
+                              decoration: const InputDecoration(
+                                labelText: "Enter new name",
+                              ),
+                            ),
+                            actions: [
+                              TextButton(
+                                child: const Text("Cancel"),
+                                onPressed: () => Navigator.of(context).pop(),
+                              ),
+                              TextButton(
+                                child: const Text("Save"),
+                                onPressed: () {
+                                  Navigator.of(context).pop(tempName);
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
+
+                      if (newName != null && newName.isNotEmpty) {
+                        try {
+                          await userRepo.updateUserName(name: newName);
+                          Fluttertoast.showToast(msg: "Name updated");
+                        } on Exception catch (e) {
+                          Fluttertoast.showToast(msg: e.toString());
+                        }
+                      }
+                    },
+                    trailing: const Icon(
+                      Icons.arrow_forward_ios,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SettingButton(
+                    title: "Change status message",
+                    subtitle: user.statusMessage ?? "",
+                    onTap: () async {
+                      final newStatus = await showDialog(
+                        context: context,
+                        builder: (context) {
+                          String tempStatus = user.statusMessage ?? "";
+                          return AlertDialog(
+                            title: const Text("Change status message"),
+                            content: TextField(
+                              onChanged: (value) {
+                                tempStatus = value;
+                              },
+                              controller:
+                                  TextEditingController(text: tempStatus),
+                              decoration: const InputDecoration(
+                                labelText: "Enter new status message",
+                              ),
+                            ),
+                            actions: [
+                              TextButton(
+                                child: const Text("Cancel"),
+                                onPressed: () => Navigator.of(context).pop(),
+                              ),
+                              TextButton(
+                                child: const Text("Save"),
+                                onPressed: () {
+                                  Navigator.of(context).pop(tempStatus);
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
+
+                      if (newStatus != null && newStatus.isNotEmpty) {
+                        try {
+                          await userRepo.updateUserStatusMessage(
+                              statusMessage: newStatus);
+                          Fluttertoast.showToast(msg: "Status updated");
+                        } on Exception catch (e) {
+                          Fluttertoast.showToast(msg: e.toString());
+                        }
+                      }
+                    },
+                    trailing: const Icon(
+                      Icons.arrow_forward_ios,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              );
+            },
           ),
         ),
       ),
