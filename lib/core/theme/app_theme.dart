f8cfcfc34f4d45bca5243aec31ddef55f1a87780
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-03-11 23:27:38 -0300

    added new colors to the theme datas

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -4,10 +4,23 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
-    primaryColor: Colors.blue,
+    primaryColor: Color(0xFF24786D),
     scaffoldBackgroundColor: Colors.white,
+    iconButtonTheme: IconButtonThemeData(
+      style: ButtonStyle(
+        iconColor: WidgetStatePropertyAll(Color(0xFF000E08)),
+      ),
+    ),
+    cardColor: const Color(0xFFF2F8F7),
     textTheme: const TextTheme(
-      bodyMedium: TextStyle(color: Colors.black),
+      bodyMedium: TextStyle(
+        color: Color(0xFF000E08),
+        fontFamily: FontFamily.caros,
+      ),
+      bodySmall: TextStyle(
+        color: Color(0xFF797C7B),
+        fontFamily: FontFamily.circular,
+      ),
     ),
   );
 
@@ -15,6 +28,12 @@ class AppTheme {
     brightness: Brightness.dark,
     primaryColor: Color(0xFF24786D),
     scaffoldBackgroundColor: Color(0xFF121414),
+    cardColor: const Color(0xFF1D2525),
+    iconButtonTheme: IconButtonThemeData(
+      style: ButtonStyle(
+        iconColor: WidgetStatePropertyAll(Colors.white),
+      ),
+    ),
     textTheme: const TextTheme(
       bodyMedium: TextStyle(
         color: Colors.white,
