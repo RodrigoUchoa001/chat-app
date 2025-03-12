4d48be719d3908c12a2c87e3fada174fa9efdc45
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-03-11 23:39:43 -0300

    added new colors to the app theme

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -5,6 +5,7 @@ class AppTheme {
   static ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
     primaryColor: Color(0xFF24786D),
+    canvasColor: Color(0xFF000E08),
     scaffoldBackgroundColor: Colors.white,
     iconButtonTheme: IconButtonThemeData(
       style: ButtonStyle(
@@ -13,6 +14,10 @@ class AppTheme {
     ),
     cardColor: const Color(0xFFF2F8F7),
     textTheme: const TextTheme(
+      titleLarge: TextStyle(
+        color: Colors.white,
+        fontFamily: FontFamily.caros,
+      ),
       bodyMedium: TextStyle(
         color: Color(0xFF000E08),
         fontFamily: FontFamily.caros,
@@ -27,6 +32,7 @@ class AppTheme {
   static ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
     primaryColor: Color(0xFF24786D),
+    canvasColor: Color(0xFF24786D),
     scaffoldBackgroundColor: Color(0xFF121414),
     cardColor: const Color(0xFF1D2525),
     iconButtonTheme: IconButtonThemeData(
@@ -35,6 +41,10 @@ class AppTheme {
       ),
     ),
     textTheme: const TextTheme(
+      titleLarge: TextStyle(
+        color: Colors.white,
+        fontFamily: FontFamily.caros,
+      ),
       bodyMedium: TextStyle(
         color: Colors.white,
         fontFamily: FontFamily.caros,
