cd21d47760765bb66d819c89d43427c20b938b06
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-03-24 09:59:28 -0300

    initializating dotenv in main function

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,13 @@ import 'package:chatapp/features/users/data/repositories/user_repository.dart';
 import 'package:chatapp/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
