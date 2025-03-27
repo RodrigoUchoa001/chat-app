acd16fbb958066594fa7d8eddeceb3f68ef2e623
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-03-26 21:34:54 -0300

    altered uploadMedia function on MediaRepositoryInterface to return nullable string

--- lib/features/media/domain/repositories/media_repository_interface.dart
+++ lib/features/media/domain/repositories/media_repository_interface.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
 
 abstract interface class MediaRepositoryInterface {
-  Future<String> uploadMedia(File mediaFile, {required bool isVideo});
+  Future<String?> uploadMedia(File mediaFile, {required bool isVideo});
 }
