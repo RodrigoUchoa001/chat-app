3c4e68ca6728bfe904d936c80ac9a78c4ff0d584
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-03-27 22:54:07 -0300

    created send media confirmation screen

---
+++ lib/features/chat/presentation/send_media_confirmation_screen.dart
@@ -0,0 +1,144 @@
+import 'dart:io';
+
+import 'package:chatapp/core/providers/firebase_auth_providers.dart';
+import 'package:chatapp/features/auth/presentation/widgets/auth_back_button.dart';
+import 'package:chatapp/features/chat/data/repositories/chat_repository.dart';
+import 'package:chatapp/features/chat/presentation/widgets/chat_icon_button.dart';
+import 'package:chatapp/features/chat/presentation/widgets/video_preview.dart';
+import 'package:chatapp/features/media/data/repositories/media_repository.dart';
+import 'package:chatapp/features/users/data/repositories/user_repository.dart';
+import 'package:chatapp/gen/assets.gen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class SendMediaConfirmationScreen extends ConsumerStatefulWidget {
+  final String chatId;
+  final String mediaFilePath;
+  const SendMediaConfirmationScreen(
+      {required this.chatId, required this.mediaFilePath, super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _SendMediaConfirmationScreenState();
+}
+
+class _SendMediaConfirmationScreenState
+    extends ConsumerState<SendMediaConfirmationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final chatRepo = ref.watch(chatRepositoryProvider);
+    final userRepo = ref.watch(userRepositoryProvider);
+    final _currentUser = ref.watch(currentUserProvider).asData?.value;
+
+    final media = File(widget.mediaFilePath);
+    final mediaFormat = media.path.split(".").last;
+    final isVideo = mediaFormat == 'mp4';
+
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          leading: AuthBackButton(),
+          title: Text(
+            "Send ${isVideo ? "Video" : "Image"}",
+            style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  fontSize: 20,
+                ),
+          ),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            if (mediaFormat == 'mp4')
+              Expanded(
+                child: Center(
+                  child: VideoPreview(media),
+                ),
+              ),
+            if (mediaFormat == 'jpg' ||
+                mediaFormat == 'png' ||
+                mediaFormat == 'jpeg')
+              Expanded(
+                child: Center(
+                  child: Image.file(media, height: 250, fit: BoxFit.cover),
+                ),
+              ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  StreamBuilder(
+                    stream: chatRepo.getChatDetails(widget.chatId),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+                      final chat = snapshot.data!;
+                      final friendId = chat.participants!.firstWhere(
+                        (id) => id != _currentUser!.uid,
+                        orElse: () => '',
+                      );
+
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Sending to:"),
+                          const SizedBox(width: 10),
+                          StreamBuilder(
+                              stream: userRepo.getUserDetails(friendId),
+                              builder: (context, snapshot) {
+                                if (snapshot.connectionState ==
+                                    ConnectionState.waiting) {
+                                  return const Center(
+                                      child: CircularProgressIndicator());
+                                }
+                                final user = snapshot.data!;
+                                return Text(
+                                  user.name!,
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodySmall!
+                                      .copyWith(
+                                        fontSize: 14,
+                                      ),
+                                );
+                              }),
+                        ],
+                      );
+                    },
+                  ),
+                  ChatIconButton(
+                    iconPath: Assets.icons.send.path,
+                    backgroundColor: Color(0xFF20A090),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _sendMedia(WidgetRef ref, File mediaFile, bool isVideo) async {
+    final mediaRepo = ref.read(mediaRepositoryProvider);
+
+    Fluttertoast.showToast(msg: "Uploading ${isVideo ? 'video' : 'image'}...");
+
+    final mediaUrl = await mediaRepo.uploadMedia(mediaFile, isVideo: isVideo);
+
+    if (mediaUrl != null) {
+      final chatRepo = ref.read(chatRepositoryProvider);
+      chatRepo.sendMessage(widget.chatId, mediaUrl, true, isVideo);
+
+      Fluttertoast.showToast(msg: "${isVideo ? 'Video' : 'Image'} sent!");
+    } else {
+      Fluttertoast.showToast(
+          msg: "Failed to upload ${isVideo ? 'video' : 'image'}");
+    }
+  }
+}
