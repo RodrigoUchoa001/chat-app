6b05b922c62202f3f78594d4276d82028c46a0e0
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-03-28 20:32:51 -0300

    created screen to view media

---
+++ lib/features/chat/presentation/view_media_screen.dart
@@ -0,0 +1,139 @@
+import 'dart:io';
+
+import 'package:chatapp/features/auth/presentation/widgets/auth_back_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:video_player/video_player.dart';
+
+class ViewMediaScreen extends ConsumerStatefulWidget {
+  final String mediaUrl;
+  const ViewMediaScreen({required this.mediaUrl, super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _ViewMediaScreenState();
+}
+
+class _ViewMediaScreenState extends ConsumerState<ViewMediaScreen> {
+  late VideoPlayerController _controller;
+  late double videoProgress;
+
+  @override
+  void initState() {
+    // _controller = VideoPlayerController.networkUrl(Uri.parse(widget.mediaUrl))
+    //   ..initialize().then(
+    //     (value) => setState(() {}),
+    //   )
+    //   ..play();
+
+    // _controller.addListener(() {
+    //   setState(() {
+    //     videoProgress = _controller.value.position.inMilliseconds.toDouble();
+    //   });
+    // });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaFormat = widget.mediaUrl.split(".").last;
+    final isVideo = mediaFormat == 'mp4';
+
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          leading: AuthBackButton(),
+          title: Text(
+            isVideo ? "Video" : "Image",
+            style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  fontSize: 20,
+                ),
+          ),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            if (mediaFormat == 'mp4')
+              Expanded(
+                child: Center(
+                  child: _controller.value.isInitialized
+                      ? AspectRatio(
+                          aspectRatio: _controller.value.aspectRatio,
+                          child: VideoPlayer(_controller),
+                        )
+                      : const Center(child: CircularProgressIndicator()),
+                ),
+              )
+            else if (mediaFormat == 'jpg' ||
+                mediaFormat == 'png' ||
+                mediaFormat == 'jpeg')
+              Expanded(
+                child: Center(
+                  child: Image.network(
+                    widget.mediaUrl,
+                    width: MediaQuery.of(context).size.width * 0.95,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  if (mediaFormat == 'mp4')
+                    Slider(
+                      value: videoProgress,
+                      min: 0.0,
+                      max: _controller.value.duration.inMilliseconds.toDouble(),
+                      onChanged: (double value) {
+                        _controller
+                            .seekTo(Duration(milliseconds: value.toInt()));
+                        setState(() {
+                          videoProgress = value;
+                        });
+                      },
+                    ),
+                  if (mediaFormat == 'mp4')
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                            "${_controller.value.position.inMinutes.remainder(60).toString().padLeft(2, '0')}:${_controller.value.position.inSeconds.remainder(60).toString().padLeft(2, '0')}"),
+                        Text(
+                            "${_controller.value.duration.inMinutes.remainder(60).toString().padLeft(2, '0')}:${_controller.value.duration.inSeconds.remainder(60).toString().padLeft(2, '0')}"),
+                      ],
+                    ),
+                  if (mediaFormat == 'mp4')
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _controller.value.isPlaying
+                              ? _controller.pause()
+                              : _controller.play();
+                        });
+                      },
+                      icon: Icon(_controller.value.isPlaying
+                          ? Icons.pause
+                          : Icons.play_arrow),
+                    ),
+                  if (mediaFormat == 'jpg' ||
+                      mediaFormat == 'png' ||
+                      mediaFormat == 'jpeg')
+                    Text(
+                      widget.mediaUrl.split('/').last,
+                      style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                            fontSize: 14,
+                          ),
+                    ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
