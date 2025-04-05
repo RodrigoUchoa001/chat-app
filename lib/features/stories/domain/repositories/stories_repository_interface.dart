f31289dbc3dc2b1c79166274b9eb18be2bf2dcf8
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-04-04 21:23:57 -0300

    updated StoriesRepositoryInterface

--- lib/features/stories/domain/repositories/stories_repository_interface.dart
+++ lib/features/stories/domain/repositories/stories_repository_interface.dart
@@ -1,10 +1,10 @@
 import 'package:chatapp/features/stories/data/dto/story_dto.dart';
 
 abstract interface class StoriesRepositoryInterface {
-  Stream<List<StoryDTO>> getStories();
+  Stream<List<StoryDTO?>> getStories();
   Stream<StoryDTO?> getStory(String storyId);
-  Future<void> sendStory();
-  Future<void> deleteStory();
-  Future<void> updateStory();
+  Future<void> sendStory(StoryDTO story);
+  Future<void> deleteStory(String storyId);
+  Future<void> updateStory(String storyId, StoryDTO story);
   Future<void> deleteAllStories();
 }
