dd3a52d9326f7c9dacfc5d1691e84dd5fbfec877
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-04-07 22:08:45 -0300

    altered functions in StoriesRepositoryInterface

--- lib/features/stories/domain/repositories/stories_repository_interface.dart
+++ lib/features/stories/domain/repositories/stories_repository_interface.dart
@@ -1,7 +1,9 @@
+import 'package:chatapp/features/auth/data/dto/user_dto.dart';
 import 'package:chatapp/features/stories/data/dto/story_dto.dart';
 
 abstract interface class StoriesRepositoryInterface {
-  Stream<List<StoryDTO?>> getStories();
+  Stream<List<UserDTO?>> getFriendsWhoHaveStories();
+  Stream<List<StoryDTO?>> getStoriesByUserId(String userId);
   Stream<StoryDTO?> getStory(String storyId);
   Future<void> sendStory(StoryDTO story);
   Future<void> deleteStory(String storyId);
