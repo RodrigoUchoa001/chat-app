3947f41c27650916f7ff0ba90d8b1ac22db3ead8
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-04-07 22:43:25 -0300

    created send story confirmation screen

---
+++ lib/features/chat/presentation/send_story_confirmation_screen.dart
@@ -0,0 +1,172 @@
+import 'dart:io';
+
+import 'package:chatapp/core/providers/firebase_auth_providers.dart';
+import 'package:chatapp/features/auth/presentation/widgets/auth_back_button.dart';
+import 'package:chatapp/features/chat/presentation/widgets/chat_icon_button.dart';
+import 'package:chatapp/features/chat/presentation/widgets/chat_text_field.dart';
+import 'package:chatapp/features/media/data/repositories/media_repository.dart';
+import 'package:chatapp/features/stories/data/dto/story_dto.dart';
+import 'package:chatapp/features/stories/data/repositories/stories_repository.dart';
+import 'package:chatapp/gen/assets.gen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:go_router/go_router.dart';
+import 'package:video_player/video_player.dart';
+
+class SendStoryConfirmationScreen extends ConsumerStatefulWidget {
+  final String mediaFilePath;
+
+  const SendStoryConfirmationScreen({
+    required this.mediaFilePath,
+    super.key,
+  });
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _SendStoryConfirmationScreenState();
+}
+
+class _SendStoryConfirmationScreenState
+    extends ConsumerState<SendStoryConfirmationScreen> {
+  late VideoPlayerController _controller;
+  late double videoProgress;
+
+  final _captionController = TextEditingController();
+
+  @override
+  void initState() {
+    videoProgress = 0.0;
+    _controller = VideoPlayerController.file(File(widget.mediaFilePath))
+      ..initialize().then((_) {
+        setState(() {
+          videoProgress = _controller.value.position.inMilliseconds.toDouble();
+        });
+      });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final media = File(widget.mediaFilePath);
+    final mediaFormat = media.path.split(".").last;
+    final isVideo = mediaFormat == 'mp4';
+
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          leading: const AuthBackButton(),
+          title: Text(
+            "Post ${isVideo ? "Video" : "Image"}",
+            style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  fontSize: 20,
+                ),
+          ),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            if (isVideo)
+              Expanded(
+                child: Center(
+                  child: Column(
+                    children: [
+                      Center(
+                        child: _controller.value.isInitialized
+                            // TODO: fix flickering when video is playing
+                            ? AspectRatio(
+                                aspectRatio: _controller.value.aspectRatio,
+                                child: VideoPlayer(_controller),
+                              )
+                            : const Center(child: CircularProgressIndicator()),
+                      ),
+                      Slider(
+                        value: videoProgress,
+                        min: 0.0,
+                        max: _controller.value.duration.inMilliseconds
+                            .toDouble(),
+                        onChanged: (double value) {
+                          _controller
+                              .seekTo(Duration(milliseconds: value.toInt()));
+                        },
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _controller.value.isPlaying
+                                ? _controller.pause()
+                                : _controller.play();
+                          });
+                        },
+                        icon: Icon(_controller.value.isPlaying
+                            ? Icons.pause
+                            : Icons.play_arrow),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            if (!isVideo)
+              Expanded(
+                child: Center(
+                  child: Image.file(media, height: 250, fit: BoxFit.cover),
+                ),
+              ),
+            ChatTextField(controller: _captionController),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: ChatIconButton(
+                  iconPath: Assets.icons.send.path,
+                  backgroundColor: const Color(0xFF20A090),
+                  onPressed: () {
+                    _sendStory(ref, media, isVideo);
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _sendStory(WidgetRef ref, File mediaFile, bool isVideo) async {
+    final mediaRepo = ref.read(mediaRepositoryProvider);
+    final storyRepo = ref.read(storiesRepositoryProvider);
+    final currentUser = ref.read(currentUserProvider).value;
+
+    Fluttertoast.showToast(msg: "Uploading ${isVideo ? 'video' : 'image'}...");
+
+    final mediaUrl = await mediaRepo.uploadMedia(mediaFile, isVideo: isVideo);
+
+    if (mediaUrl != null && currentUser != null) {
+      await storyRepo.sendStory(
+        StoryDTO(
+          createdAt: DateTime.now().toString(),
+          userId: currentUser.uid,
+          mediaURL: mediaUrl,
+          mediaType: isVideo ? "video" : "image",
+          expiresAt: DateTime.now().add(const Duration(days: 1)).toString(),
+          views: [],
+          caption: _captionController.text,
+        ),
+      );
+
+      Fluttertoast.showToast(msg: "Story posted!");
+      context.pop();
+    } else {
+      Fluttertoast.showToast(msg: "Failed to upload story");
+    }
+  }
+}
