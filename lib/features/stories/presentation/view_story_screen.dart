8ed1e90093e1a80fc93dd4e15474819e4be0902e
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-04-09 09:12:54 -0300

    added to the view story screen story image background, StoryProgressBar, and user info

--- lib/features/stories/presentation/view_story_screen.dart
+++ lib/features/stories/presentation/view_story_screen.dart
@@ -1,3 +1,5 @@
+import 'package:chatapp/features/chat/presentation/widgets/chat_profile_pic.dart';
+import 'package:chatapp/features/stories/presentation/widgets/story_progress_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -14,7 +16,81 @@ class _ViewStoryScreenState extends ConsumerState<ViewStoryScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(),
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Center(
+              child: Image(
+                image: NetworkImage(
+                  'https://picsum.photos/200/300',
+                ),
+                fit: BoxFit.cover,
+                height: double.infinity,
+              ),
+            ),
+            Container(
+              height: 60,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.black.withAlpha(200), Colors.transparent],
+                ),
+              ),
+              child: Column(
+                children: [
+                  const SizedBox(height: 4),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 4),
+                    child: StoryProgressBar(
+                      totalStories: 5,
+                      currentIndex: 2,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: Row(
+                      children: [
+                        ChatProfilePic(
+                          isOnline: true,
+                          avatarRadius: 45,
+                        ),
+                        const SizedBox(width: 8),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Username',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodySmall!
+                                  .copyWith(
+                                    fontSize: 18,
+                                    color: Colors.white,
+                                  ),
+                            ),
+                            Text(
+                              '40min ago',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodySmall!
+                                  .copyWith(
+                                    fontSize: 12,
+                                    color: Colors.white.withAlpha(200),
+                                  ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
