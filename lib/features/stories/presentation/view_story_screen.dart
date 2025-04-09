feb66bae49c8a5b77d6a407eccad54e9ee909f01
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-04-09 09:26:55 -0300

    added space to show story caption on view story screen

--- lib/features/stories/presentation/view_story_screen.dart
+++ lib/features/stories/presentation/view_story_screen.dart
@@ -28,65 +28,84 @@ class _ViewStoryScreenState extends ConsumerState<ViewStoryScreen> {
                 height: double.infinity,
               ),
             ),
-            Container(
-              height: 60,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [Colors.black.withAlpha(200), Colors.transparent],
+            Column(
+              children: [
+                Expanded(
+                  child: Stack(
+                    children: [
+                      Container(
+                        height: 100,
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                              Colors.black.withAlpha(200),
+                              Colors.transparent
+                            ],
+                          ),
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          const SizedBox(height: 4),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 4),
+                            child: StoryProgressBar(
+                              totalStories: 5,
+                              currentIndex: 2,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 8),
+                            child: Row(
+                              children: [
+                                ChatProfilePic(
+                                  isOnline: true,
+                                  avatarRadius: 45,
+                                ),
+                                const SizedBox(width: 8),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Username',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodySmall!
+                                          .copyWith(
+                                            fontSize: 18,
+                                            color: Colors.white,
+                                          ),
+                                    ),
+                                    Text(
+                                      '40min ago',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodySmall!
+                                          .copyWith(
+                                            fontSize: 12,
+                                            color: Colors.white.withAlpha(200),
+                                          ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              child: Column(
-                children: [
-                  const SizedBox(height: 4),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 4),
-                    child: StoryProgressBar(
-                      totalStories: 5,
-                      currentIndex: 2,
-                    ),
+                Container(
+                  color: Colors.black.withAlpha(100),
+                  child: const Center(
+                    child: Text('Story Caption'),
                   ),
-                  const SizedBox(height: 8),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8),
-                    child: Row(
-                      children: [
-                        ChatProfilePic(
-                          isOnline: true,
-                          avatarRadius: 45,
-                        ),
-                        const SizedBox(width: 8),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Username',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodySmall!
-                                  .copyWith(
-                                    fontSize: 18,
-                                    color: Colors.white,
-                                  ),
-                            ),
-                            Text(
-                              '40min ago',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodySmall!
-                                  .copyWith(
-                                    fontSize: 12,
-                                    color: Colors.white.withAlpha(200),
-                                  ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             )
           ],
         ),
