4fa9eb49b6b69a1ce429652970f8985e5e510469
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-04-11 09:59:08 -0300

    now the calculateTimeSinceLastMessage use the device language

--- lib/features/chat/presentation/utils/calculate_time_since_last_message.dart
+++ lib/features/chat/presentation/utils/calculate_time_since_last_message.dart
@@ -1,13 +1,19 @@
+import 'package:chatapp/core/localization/app_localization.dart';
+import 'package:chatapp/core/localization/locale_provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 
-String calculateTimeSinceLastMessage(dynamic timestamp) {
+String calculateTimeSinceLastMessage(dynamic timestamp, WidgetRef ref) {
+  final locale = ref.watch(localeProvider);
+  final localization = ref.watch(localizationProvider(locale)).value;
+
   if (timestamp is Timestamp) {
     timestamp = timestamp.toDate();
   } else if (timestamp is String) {
     timestamp = DateTime.parse(timestamp.toString());
   } else if (timestamp is! DateTime) {
-    return "Invalid date";
+    return localization?.translate("invalid-date") ?? "";
   }
 
   final now = DateTime.now();
@@ -15,13 +21,17 @@ String calculateTimeSinceLastMessage(dynamic timestamp) {
 
   if (difference.inHours < 24) {
     if (difference.inMinutes < 1) {
-      return "Just now";
+      return localization?.translate("just-now") ?? "";
     } else if (difference.inMinutes < 60) {
-      return "${difference.inMinutes} min ago";
+      return "${difference.inMinutes} min ${localization?.translate("ago") ?? ""}";
     } else {
-      return "${difference.inHours}h ago";
+      return "${difference.inHours}h ${localization?.translate("ago") ?? ""}";
     }
   } else {
-    return DateFormat("MMM d, yyyy").format(timestamp);
+    if (locale.languageCode == "pt") {
+      return DateFormat("dd/MM/yyyy").format(timestamp);
+    } else {
+      return DateFormat("MMM d, yyyy").format(timestamp);
+    }
   }
 }
