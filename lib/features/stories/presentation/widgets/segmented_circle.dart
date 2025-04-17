e9895564717cd3af645465f88f120e37911d104e
Rodrigo Uchôa <franciscorferreira@aluno.uespi.br>
2025-04-16 22:30:04 -0300

    added to the segmented circle widget functions to show diferent borders if there is no story or if its only one

--- lib/features/stories/presentation/widgets/segmented_circle.dart
+++ lib/features/stories/presentation/widgets/segmented_circle.dart
@@ -1,7 +1,9 @@
 import 'dart:math';
+import 'package:chatapp/core/theme/is_dark_mode.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SegmentedCircle extends StatelessWidget {
+class SegmentedCircle extends ConsumerWidget {
   final int segmentCount;
   final double radius;
   final double strokeWidth;
@@ -18,7 +20,28 @@ class SegmentedCircle extends StatelessWidget {
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    if (segmentCount == 1) {
+      return Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.white,
+            width: strokeWidth,
+          ),
+          borderRadius: BorderRadius.circular(50),
+        ),
+        child: Container(
+          width: 58,
+          height: 58,
+          padding: EdgeInsets.all(strokeWidth),
+          child: Padding(
+            padding: const EdgeInsets.all(6),
+            child: ClipOval(child: child),
+          ),
+        ),
+      );
+    }
+
     return CustomPaint(
       painter: _SegmentedCirclePainter(
         segmentCount: segmentCount,
@@ -26,13 +49,23 @@ class SegmentedCircle extends StatelessWidget {
         color: color,
       ),
       child: Container(
-        width: radius * 2,
-        height: radius * 2,
+        width: 58,
+        height: 58,
         padding: EdgeInsets.all(strokeWidth),
+        decoration: segmentCount > 0
+            //  only show border if there are no stories
+            ? null
+            : BoxDecoration(
+                border: Border.all(
+                  color: isDarkMode(ref, context)
+                      ? Color(0xFF4B9289)
+                      : Color(0xFF363F3B),
+                  width: 1,
+                ),
+                borderRadius: BorderRadius.circular(50),
+              ),
         child: Padding(
-          padding: segmentCount > 0
-              ? const EdgeInsets.all(6)
-              : const EdgeInsets.all(0),
+          padding: const EdgeInsets.all(6),
           child: ClipOval(child: child),
         ),
       ),
